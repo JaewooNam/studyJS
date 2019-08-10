0558417349696d3cfb60ea9915b57cfa759784f5
JaewooNam <namssabu12@gmail.com>
2019-08-10 10:37:29 +0900

    Add reverse string

---
+++ Algorithm/DP/1_2_3_totalSum.js
@@ -0,0 +1,6 @@
+let memo
+function dp(n) {
+  
+}
+
+console.log(dp(10))
---
+++ Algorithm/reverseString.js
@@ -0,0 +1,5 @@
+function reverseString(str) {
+  return str.split('').reverse().join('')
+}
+
+console.log(reverseString('I want to be a good front end developer'))
