384f762bc4e9ee26a49e1eae13a7fe0cdcd1c7ae
JaewooNam <namssabu12@gmail.com>
2019-08-21 15:58:17 +0900

    Recover origin file

--- JS/bind.js
+++ JS/bind.js
@@ -1,5 +1,11 @@
-const a = undefined
+// 새 함수 인스턴스를 만드는데 그 this는 bind()에 전달된 값.
 
-if (a) {
- console.log('1')
+window.color = 'red'
+const o = { color: 'blue' }
+
+function sayColor() {
+  console.log(this.color)
 }
+
+const objectSayColor = sayColor.bind(o)
+objectSayColor() // blue
